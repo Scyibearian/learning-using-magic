503a95f78243ff6f0595ddd4bb729cbfdf745fe9
Scyibearian <cyberboy02@gmail.com>
2022-08-09 11:44:13 +0100

    Update settings.py

--- learningusingmagicproject/settings.py
+++ learningusingmagicproject/settings.py
@@ -15,13 +15,15 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
-    SECRET_KEY = f.read().strip()
+#with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
+#    SECRET_KEY = f.read().strip()
+
+SECRET_KEY = os.environ.get('DJANGO_LUM_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://localhost:8000/", "http://127.0.0.1:8000/", "learningusingmagic.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "learningusingmagic.herokuapp.com"]
 
 
 # Application definition
