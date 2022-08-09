41dee8c04d0c8ecf6452567e3c6bc21b916de0b3
Scyibearian <cyberboy02@gmail.com>
2022-08-09 15:38:41 +0100

    django-heroku gunicorn set up

--- learningusingmagicproject/settings.py
+++ learningusingmagicproject/settings.py
@@ -7,6 +7,8 @@ import os
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 print(BASE_DIR)
@@ -131,3 +133,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
