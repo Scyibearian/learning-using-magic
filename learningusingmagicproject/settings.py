9c11b54627c107c81b65c9822f33b64d780d4c37
Scyibearian <cyberboy02@gmail.com>
2022-08-11 16:41:38 +0100

    Change database engine

--- learningusingmagicproject/settings.py
+++ learningusingmagicproject/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'learningusingmagicproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'test',
+        #'USER': 'postgres',
+        #'PASSWORD': 'test123',
+        #'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
